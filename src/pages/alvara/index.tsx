fb95fdde7b65f960fcf4b82a40372010a290d0e8
Filipe Mendes Webber <filipewebber@filipewebber.dev>
2022-06-24 21:25:44 -0300

    Fix alignment

--- src/pages/alvara/index.tsx
+++ src/pages/alvara/index.tsx
@@ -15,49 +15,55 @@ export interface AlvaraProps {
 const Alvara: React.FunctionComponent<AlvaraProps> = ({ host, nome, rg, estilos, cidade, data, validade }) => (
   <Box
     width="740px"
-    height="470px"
+    height="460px"
     padding="45px"
+    margin="0"
     bgColor="rgb(242, 243, 230)"
     backgroundImage={`http://${host}/borda-alvara.png`}
     bgRepeat="no-repeat"
     bgSize="contain"
     bgPosition="center"
   >
-    <Flex direction="column" alignItems="center" className="miolo">
+    <Flex direction="column" alignItems="center" className="miolo" width="full">
       <Box margin="0">
-        <Image src={`http://${host}/logo.png`} width="150px" alt="logo" />
+        <Image src={`http://${host}/logo.png`} width="130px" alt="logo" />
       </Box>
-      <Heading margin="0" padding="15px 0" as="h1" fontSize="45px" fontWeight="400">
+      <Heading margin="0" padding="15px 0" as="h1" fontSize="35px" fontWeight="400">
         ALVARÁ
       </Heading>
-      <Text width="full" textAlign="center" margin="0 35px" fontSize="20px">
+      <Text width="full" textAlign="center" padding="0 35px" margin="0" fontSize="20px" lineHeight="1.1">
         O presidente da federação paranaense de kung fu wushu (FPRKW) no uso dos atributos que lhe conferem autoriza o SR(A){' '}
-        <b> {nome} </b>, portador do RG {rg} SSP/SP, a ministrar aulas de Kung Fu / wushu nos estilos estilos {estilos}.
+        <b>{nome}</b>, portador do RG {rg} SSP/SP, a ministrar aulas de Kung Fu / wushu nos estilos estilos {estilos}.
       </Text>
-      <Flex justifyContent="space-between" alignItems="flex-end">
+      <Flex justifyContent="space-between" alignItems="flex-end" height="full" width="full">
         <Box>
-          <Box style={{ width: '90px' }} />
+          <Box style={{ width: '150px' }} />
         </Box>
-        <Box>
-          <Box className="div-assinatura">
+        <Flex direction="column" justifyContent="center" alignItems="center" textAlign="center" width="400px">
+          <Box style={{ padding: '30px' }}>
             <b>
               {cidade}, {data}
             </b>
             <br />
-            <b>validade de {validade}</b>
+            <b>Validade de {validade}</b>
           </Box>
 
           <Box>__________________________</Box>
-          <Box style={{ padding: '25px' }}>
-            <b> Aldebran Valentim </b>
-            <br />
-            Presidente da Federação Paranaense de <br />
-            Kung Fu Wushu
+          <Box style={{ padding: '15px' }}>
+            <Text fontSize="15px" lineHeight="1.1" fontWeight="700" margin="0">
+              Aldebran Valentim
+            </Text>
+            <Text fontSize="15px" lineHeight="1.1" fontWeight="400" margin="0">
+              Presidente da Federação Paranaense de
+            </Text>
+            <Text fontSize="15px" lineHeight="1.1" fontWeight="400" margin="0">
+              Kung Fu Wushu
+            </Text>
           </Box>
-        </Box>
-        <Box>
+        </Flex>
+        <Flex justifyContent="flex-end" alignItems="flex-end" width="150px">
           <Image src={`http://${host}/www.png`} width="90px" alt="Landscape picture" />
-        </Box>
+        </Flex>
       </Flex>
     </Flex>
   </Box>
